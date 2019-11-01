7186ab6ec363d33408e7c444067d9a91a6879e84
wanghy96 <33179834+wanghy96@users.noreply.github.com>
2019-11-01 20:39:08 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ sudo pip3 install requests bs4 lxml xlwt xlrd django yagmail jupyter pyecharts
 echo "export PATH=$PATH:~/.local/bin" | sudo tee -a /etc/profile
 source /etc/profile
 jupyter notebook --generate-config
-jupyter notebook password
 sudo cp jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+jupyter notebook password
 sudo cp rc.local /etc/
 sudo chmod +x /etc/rc.local
 sudo cp dhcpcd.conf /etc/
