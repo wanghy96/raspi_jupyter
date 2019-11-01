8971e700f5a66f9c4eb915e310c65d7f04ec65ab
wanghy96 <33179834+wanghy96@users.noreply.github.com>
2019-11-01 20:35:38 +0800

    Add files via upload

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -337,7 +337,7 @@ c.NotebookApp.port = 8888
 #  
 #  Setting to an empty string disables authentication altogether, which is NOT
 #  RECOMMENDED.
-c.NotebookApp.token = ' '
+c.NotebookApp.token = ''
 
 ## Supply overrides for the tornado.web.Application that the Jupyter notebook
 #  uses.
