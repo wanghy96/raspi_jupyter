3eb273ecd12ba76f2433aaee9536d489dbca3a89
wanghy96 <33179834+wanghy96@users.noreply.github.com>
2019-11-01 20:54:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-git clone https://github.com/wanghy96/raspi_jupyter.git
 sudo pip3 install --upgrade jupyter-core
 sudo pip3 install --upgrade jupyter-client
 sudo pip3 install requests bs4 lxml xlwt xlrd django yagmail jupyter pyecharts
 echo "export PATH=$PATH:~/.local/bin" | sudo tee -a /etc/profile
 source /etc/profile
 jupyter notebook --generate-config
-sudo cp raspi_jupyter/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+sudo cp jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
 yes|jupyter notebook password
-sudo cp raspi_jupyter/rc.local /etc/
+sudo cp rc.local /etc/
 sudo chmod +x /etc/rc.local
-sudo cp raspi_jupyter/dhcpcd.conf /etc/
-sudo cp raspi_jupyter/speak_ip.py /var/speak_raspi_ip/
+sudo cp dhcpcd.conf /etc/
+sudo cp speak_ip.py /var/speak_raspi_ip/
 echo 'finished!'
